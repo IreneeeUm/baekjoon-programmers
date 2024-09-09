2e810fe459505b2ef7d2182e0b2dfe1c44ebe146
IreneeeUm <167860645+IreneeeUm@users.noreply.github.com>
2024-09-09 20:06:30 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 27.59 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        String wm = "수박";
+        for(int i=0; i<n/2; i++){
+            answer += wm;
+        }
+        if(n % 2 != 0){
+            answer += "수";
+        }
+        return answer;
+    }
+}
